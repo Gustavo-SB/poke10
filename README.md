901478c895a717a396af4b0f345ebde0f730a09d
gabrielarroyoc <gabrielarroyoc18@gmail.com>
2022-06-16 23:51:48 -0300

    :art: style: color palette

---
+++ README.md
@@ -0,0 +1,8 @@
+### ALL PAGES - Sprint 1 (2 SEMANAS - 30/06)
+
+| Name           | Description                                                           | Status |
+| -------------- | --------------------------------------------------------------------- | ------ |
+| Home           | Hero with visual details & access to Login Page User - it takes X hrs | ⚠      |
+| About          | Content with description about project - it takes X hrs               | ⚠      |
+| Login          | Page with Login Authentication for User - it takes X hrs              | ⚠      |
+| Pages/Pokémons | Custom Page about Pokémon with more stats - it takes X hrs            | ⚠      |
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,8 +1,42 @@
-import { extendTheme } from "@chakra-ui/react";
+import { background, extendTheme } from "@chakra-ui/react";
 import * as ReactDOMimport from 'react-dom/client';
 
 const colors = {
+    backgroundColor: {
+        500: '#242357',
+        600: '#242357',
+        700: '#242357',
+        800: '#242357',
+        900: '#242357',
+    },
+    PrimaryColor: {
+        500: "#5856D6",
+        600: "#5856D6",
+        700: "#5856D6",
+        800: "#5856D6",
+        900: "#5856D6",
+    SecondaryColor: {
+        500: "#5D5BE3",
+        600: "#5D5BE3",
+        700: "#5D5BE3",
+        800: "#5D5BE3",
+        900: "#5D5BE3",
+    },
+    TertiaryColor: {
+        500: "#4E4CBD",
+        600: "#4E4CBD",
+        700: "#4E4CBD",
+        800: "#4E4CBD",
+        900: "#4E4CBD",
+    },
+    QuaternaryColor: {
+        500: "#3E3C96",
+        600: "#3E3C96",
+        700: "#3E3C96",
+        800: "#3E3C96",
+    }
 
+}
 }
 
 export const theme = extendTheme({ colors });
